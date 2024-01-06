ae64a0d715de61410ebd9c4e76753b1d90dd06bb
Habteab <Habteab@habteab>
2024-01-05 19:12:35 -0500

    this is initial commit

---
+++ src/demo/TestDemo.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class TestDemo {
+ public static void main(String[] args) {
+	
+	 //local repository
+	 System.out.println("Demo test: crating project1");
+	 System.out.println(" the test is going good...");
+}
+}
