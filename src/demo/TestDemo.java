6dfb3c6961a93a8b93c415e5b70b9f235b37edb2
MebratTikabo <155407490+MebratTikabo@users.noreply.github.com>
2024-01-05 19:14:39 -0500

    Update TestDemo.java

--- src/demo/TestDemo.java
+++ src/demo/TestDemo.java
@@ -7,4 +7,8 @@ public class TestDemo {
 	 System.out.println("Demo test: crating project1");
 	 System.out.println(" the test is going good...");
 }
+//remote repository
+	public void displayThis(){
+		System.out.println("this one is for test");
+	}
 }
